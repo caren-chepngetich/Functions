f2b6e51647ac6b387437b5dd4c2ed730d7d32d3c
Student <Caren-Chepngetich>
2024-02-23 12:39:43 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+
+
+
+fun main(){
+    println("Hello Caren")
+    modulus(5,3)
+    sumOfNumbers(9,30,35,68)
+    myFunFact("I am an ambivert who loves cooking")
+
+}
+fun modulus(num1: Int, num2: Int){
+    var result=num1 % num2
+    println(result)
+}
+fun sumOfNumbers(num1: Int, num2: Int,num3: Int, num4: Int){
+var sum=num1+num2+num3+num4
+    println(sum)}
+
+fun myFunFact(funfact:String){
+println(funfact)
+}
+
+
+
+
+
+
